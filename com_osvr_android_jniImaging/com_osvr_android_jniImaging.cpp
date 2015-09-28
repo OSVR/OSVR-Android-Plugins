06fe76d738cdcc351a82265451abbf1c51804588
JeroMiya <bell.jeremy@gmail.com>
2015-09-28 18:23:34 -0400

    Updated implementation of the jni imaging plugin. Adds YUV420SP decoding to RGB, removes the RawImaging* classes and uses the OpenCV based wrappers instead.

--- com_osvr_android_jniImaging/com_osvr_android_jniImaging.cpp
+++ com_osvr_android_jniImaging/com_osvr_android_jniImaging.cpp
@@ -24,9 +24,10 @@
 
 // Internal Includes
 #include <jni.h>
+#include <android/log.h>
 
 #include <osvr/PluginKit/PluginKit.h>
-#include <osvr/PluginKit/ImagingInterfaceC.h>
+#include <osvr/PluginKit/ImagingInterface.h>
 #include <osvr/Util/ChannelCountC.h>
 #include <osvr/PluginKit/DeviceInterface.h>
 #include <osvr/Util/StringLiteralFileToString.h>
@@ -42,129 +43,44 @@
 #include <sstream>
 #include <cstring>
 
-OSVR_ImageBufferElement *gLastFrame = NULL;
-OSVR_ImagingMetadata gLastFrameMetadata;
+#define  LOG_TAG    "libgl2jni"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
-extern "C" {
-    JNIEXPORT void JNICALL Java_com_osvr_android_JniBridge_reportFrame(JNIEnv * env, jclass clazz,
-      jbyteArray data, jlong width, jlong height, jshort channels, jshort depth);
-}
+extern OSVR_ImageBufferElement *gLastFrame;
+extern OSVR_ImagingMetadata gLastFrameMetadata;
 
-JNIEXPORT void JNICALL Java_com_osvr_android_JniBridge_reportFrame(JNIEnv * env, jclass clazz,
-    jbyteArray data, jlong width, jlong height, jshort channels, jshort depth) {
+// based off of code from here: http://www.java2s.com/Open-Source/Android_Free_Code/WallPaper/screen/com_yaji_viewfinderImageUtil_java.htm
+// which in turn was based off the android open source project. Both Apache licensed.
+static void decodeYUV420SP(OSVR_ImageBufferElement* rgb, OSVR_ImageBufferElement* yuv420sp, int width, int height) {
+  int frameSize = width * height;
 
-    gLastFrameMetadata.height = (OSVR_ImageDimension)height;
-    gLastFrameMetadata.width = (OSVR_ImageDimension)width;
-    gLastFrameMetadata.channels = (OSVR_ImageChannels)channels;
-    gLastFrameMetadata.depth = (OSVR_ImageDepth)depth;
+  for (int j = 0, yp = 0; j < height; j++) {
+    int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;
+    for (int i = 0; i < width; i++, yp++) {
+      int y = (0xff & ((int) yuv420sp[yp])) - 16;
+      if (y < 0) y = 0;
+      if ((i & 1) == 0) {
+        v = (0xff & yuv420sp[uvp++]) - 128;
+        u = (0xff & yuv420sp[uvp++]) - 128;
+      }
 
-    // @todo determine whether the current metadata matches the last metadata,
-    // and if so, reuse the last frame buffer instead of deleting and recreating.
-    // better yet, use a ring buffer so that image reports aren't lost if update
-    // isn't called frequently enough.
-    if(gLastFrame) {
-        // this image will be lost. the plugin's update method wasn't called
-        // fast enough.
-        delete[] gLastFrame;
+      int y1192 = 1192 * y;
+      int r = (y1192 + 1634 * v);
+      int g = (y1192 - 833 * v - 400 * u);
+      int b = (y1192 + 2066 * u);
+
+      if (r < 0) r = 0; else if (r > 262143) r = 262143;
+      if (g < 0) g = 0; else if (g > 262143) g = 262143;
+      if (b < 0) b = 0; else if (b > 262143) b = 262143;
+
+      rgb[yp * 3] = r;
+      rgb[yp * 3 + 1] = g;
+      rgb[yp * 3 + 2] = b;
+      //rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
     }
-    int size = env->GetArrayLength(data);
-    gLastFrame = new OSVR_ImageBufferElement[size];
-    env->GetByteArrayRegion(data, 0, size, reinterpret_cast<jbyte*>(gLastFrame));
+  }
 }
-
-namespace osvr {
-namespace pluginkit {
-    /** @defgroup PluginKitCppImaging Imaging interface (C++)
-        @brief Sending image reports from a device in your plugin.
-        @ingroup PluginKit
-
-        @{
-    */
-
-    /// @brief A class wrapping a OSVR_ImageBufferElement* representing a frame, as well as the
-    /// sensor ID it corresponds to. Pass to
-    /// osvr::pluginkit::DeviceToken::send() along with your
-    /// osvr::pluginkit::RawImagingInterface.
-    class RawImagingMessage {
-      public:
-        /// @brief Constructor, optionally taking a sensor number. Does NOT clone the
-        /// supplied image/data, but does take ownership of the pointer passed.
-        RawImagingMessage(OSVR_ImageBufferElement *frame, OSVR_ImagingMetadata frameMetadata, OSVR_ChannelCount sensor = 0)
-            : m_frame(frame), m_frameMetadata(frameMetadata), m_sensor(sensor) {
-        }
-
-        /// @brief Retrieves a reference to the internal (cloned) data.
-        OSVR_ImageBufferElement *getFrame() const { return m_frame; }
-
-        /// @brief Retrieves the frame metadata.
-        OSVR_ImagingMetadata getFrameMetadata() const { return m_frameMetadata; }
-
-        /// @brief Gets the sensor number.
-        OSVR_ChannelCount getSensor() const { return m_sensor; }
-
-      private:
-        OSVR_ImageBufferElement *m_frame;
-        OSVR_ImagingMetadata m_frameMetadata;
-        OSVR_ChannelCount m_sensor;
-    };
-
-    /// @brief A class wrapping an imaging interface for a device.
-    class RawImagingInterface {
-      public:
-        /// @brief Default constructor or constructor from an existing
-        /// OSVR_ImagingDeviceInterface (assumed to be registered with an
-        /// OSVR_DeviceInitOptions already)
-        RawImagingInterface(OSVR_ImagingDeviceInterface iface = NULL)
-            : m_iface(iface) {}
-
-        /// @brief Constructor that registers an imaging interface with the
-        /// device init options object.
-        explicit RawImagingInterface(OSVR_DeviceInitOptions opts,
-                                  OSVR_ChannelCount numSensors = 1) {
-            OSVR_ReturnCode ret =
-                osvrDeviceImagingConfigure(opts, &m_iface, numSensors);
-            if (OSVR_RETURN_SUCCESS != ret) {
-                throw std::logic_error("Could not initialize an Imaging "
-                                       "Interface with the device options "
-                                       "given!");
-            }
-        }
-
-        /// @brief Send method - usually called by
-        /// osvr::pluginkit::DeviceToken::send()
-        void send(DeviceToken &dev, const RawImagingMessage &message,
-                  OSVR_TimeValue const &timestamp) {
-            if (!m_iface) {
-                throw std::logic_error(
-                    "Must initialize the imaging interface before using it!");
-            }
-            OSVR_ImagingMetadata metadata = message.getFrameMetadata();
-
-            // metadata.channels = frame.channels();
-            // metadata.depth = typedata.getSize();
-            // metadata.width = frame.cols;
-            // metadata.height = frame.rows;
-            // metadata.type = typedata.isFloatingPoint()
-            //                     ? OSVR_IVT_FLOATING_POINT
-            //                     : (typedata.isSigned() ? OSVR_IVT_SIGNED_INT
-            //                                            : OSVR_IVT_UNSIGNED_INT);
-
-            OSVR_ReturnCode ret =
-                osvrDeviceImagingReportFrame(dev, m_iface,
-                    message.getFrameMetadata(), message.getFrame(), message.getSensor(),
-                    &timestamp);
-
-            if (OSVR_RETURN_SUCCESS != ret) {
-                throw std::runtime_error("Could not send imaging message!");
-            }
-        }
-
-      private:
-        OSVR_ImagingDeviceInterface m_iface;
-    };
-    /// @}
-} // namespace pluginkit
-} // namespace osvr
 
 // Anonymous namespace to avoid symbol collision
 namespace {
@@ -172,16 +88,16 @@ namespace {
     class AndroidJniImagingDevice {
     public:
         AndroidJniImagingDevice(OSVR_PluginRegContext ctx, int cameraNum = 0, int channel = 0)
-          : m_camera(cameraNum), m_channel(channel) {
+          : m_camera(cameraNum), m_channel(channel), m_reportNumber(0) {
             // @todo sanity check for constructor arguments. All ptrs have to
             // be non-null
-
+            //LOGI("[OSVR] AndroidJniImagingDevice instantiated");
             /// Create the initialization options
             OSVR_DeviceInitOptions opts = osvrDeviceCreateInitOptions(ctx);
 
             /// Configure an imaging interface (with the default number of sensors,
             /// 1)
-            m_imaging = osvr::pluginkit::RawImagingInterface(opts);
+            m_imaging = osvr::pluginkit::ImagingInterface(opts);
 
             /// Come up with a device name
             std::ostringstream os;
@@ -202,6 +118,7 @@ namespace {
         }
 
         OSVR_ReturnCode update() {
+          //LOGI("[OSVR] AndroidJniImagingDevice::update # %d called", m_reportNumber++);
 
           // Get a timestamp for the upcoming camera grab.
           osvr::util::time::TimeValue frameTime = osvr::util::time::getNow();
@@ -210,11 +127,20 @@ namespace {
           // Note that if larger than 160x120 (RGB), will used shared memory
           // backend only.
           if(gLastFrame) {
-              m_dev.send(m_imaging, osvr::pluginkit::RawImagingMessage(gLastFrame, gLastFrameMetadata),
-                  frameTime);
+              LOGI("[OSVR] image report # %d called", m_reportNumber++);
+              OSVR_ImageBufferElement* buffer
+                  = new OSVR_ImageBufferElement[gLastFrameMetadata.width * gLastFrameMetadata.height * gLastFrameMetadata.channels];
+
+              decodeYUV420SP(buffer, gLastFrame, gLastFrameMetadata.width, gLastFrameMetadata.height);
+
+              cv::Mat frame(gLastFrameMetadata.width, gLastFrameMetadata.height, CV_8UC3, buffer);
+
+              m_dev.send(m_imaging, osvr::pluginkit::ImagingMessage(frame, 3),
+                   frameTime);
 
               // The interface takes ownership of the image data. Also, we don't
               // want to send the image more than once.
+              delete[] gLastFrame;
               gLastFrame = NULL;
           }
 
@@ -224,10 +150,11 @@ namespace {
     private:
 
       osvr::pluginkit::DeviceToken m_dev;
-      osvr::pluginkit::RawImagingInterface m_imaging;
+      osvr::pluginkit::ImagingInterface m_imaging;
       int m_camera;
       int m_channel;
       OSVR_ImageBufferElement * m_frame;
+      int m_reportNumber;
     };
 
     class HardwareDetection {
